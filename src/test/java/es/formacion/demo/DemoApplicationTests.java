a82042239cfc42d59464ad2c5c92cb6f8741d758
Oriol Canadés <oriol.canades@in2.es>
2024-12-14 13:00:28 +0100

    Update Test class (commented)

--- src/test/java/es/formacion/demo/DemoApplicationTests.java
+++ src/test/java/es/formacion/demo/DemoApplicationTests.java
@@ -1,14 +1,14 @@
-package es.formacion.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-		// This method is empty and does not need code
-	}
-
-}
+//package es.formacion.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DemoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//		// This method is empty and does not need code
+//	}
+//
+//}
