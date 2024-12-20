2e131b2dcb12cf25056f3a136a2d18d32d29a9a0
Sebastián Rodríguez <sebas517@hotmail.com>
2024-12-20 20:14:54 +0100

    Added method for: Get all users. Update user by id Delete user by id

--- src/main/java/es/formacion/demo/service/impl/AppUserServiceImpl.java
+++ src/main/java/es/formacion/demo/service/impl/AppUserServiceImpl.java
@@ -9,8 +9,10 @@ import es.formacion.demo.service.AppUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 @Slf4j
 @Service
@@ -45,5 +47,49 @@ public class AppUserServiceImpl implements AppUserService {
             throw new AppUserNotFoundException("User not found");
         }
     }
+    /**
+     * The getAllUsers function returns a list of users.
+     * The list of users returned by AppUsersResponse.findAll() 
+     * is a DTO with the ID and name stored in a list, and this list is stored in a map
+     */
 
+    @Override
+    public List<AppUserResponse> getAllUsers() {
+        log.info("Getting all users");
+        List<AppUser> users = StreamSupport
+        .stream(appUserRepository.findAll().spliterator(),false).toList();
+        if(appUserRepository.count()!=0){
+            return users.stream().map(user->new AppUserResponse(
+                String.valueOf(user.getId()), user.getUsername())).toList();
+        }else{
+            throw new AppUserNotFoundException("No users found");
+        }
+        
+    }
+
+    /**
+     * I intended to call the getUserById() function, but it returns a DTO, 
+     * and I would need it to return an AppUser Object. 
+     * So I will look for the user by id.
+     */
+
+    @Override
+    public AppUserResponse updateAppUserById(Long id, AppUserRequest appUserRequest) {
+        log.info("Updating user with id:{}.", id);
+        AppUser appUser = appUserRepository.findById(id).orElseThrow(()->new AppUserNotFoundException("User with ID: "+id+" not found."));
+        appUser.setUsername(appUserRequest.username());
+        appUser.setPassword(String.valueOf(appUserRequest.password()));
+        appUserRepository.save(appUser);
+        log.info("User whith ID:{}. updated", id);
+
+        return new AppUserResponse(String.valueOf(appUser.getId()), appUser.getUsername());
+    }
+
+    @Override
+    public void deleteUserById(Long id){
+        log.info("Deleting user with id:{}", id);
+        AppUser appUser = appUserRepository.findById(id).orElseThrow(()->new AppUserNotFoundException("User with ID: "+id+" not found."));
+        appUserRepository.delete(appUser);
+        log.info("User with ID:{}. deleted", id);
+    }
 }
